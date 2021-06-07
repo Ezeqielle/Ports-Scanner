80e45e2d3d8160757971ec00256a12bbc858ea5c
mathis di mascio <mathis.alex.dim@gmail.com>
2021-06-07 15:05:55 +0200

    add scann all ports

--- handrun_portscanner.py
+++ handrun_portscanner.py
@@ -5,6 +5,38 @@ import optparse
 from threading import *
 from termcolor import colored
 
+####### All ports #######
+
+def multiConnScan(tgtHost,tgtPort):
+    try:
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.connect((tgtHost, tgtPort))
+        print(colored(f'[+] {tgtPort}/tcp Open', 'green'))
+    except:
+        pass
+    finally:
+        sock.close()
+
+def multiPortsScanner(tgtHost):
+    try:
+        tgtIP = gethostbyname(tgtHost)
+    except:
+        print(colored(f'[-] Unknow host {tgtHost}', 'red'))
+
+    try:
+        tgtName = gethostbyaddr(tgtIP)
+        print(colored(f'[+] Scan result for: {tgtName(0)}', 'yellow'))
+    except:
+        print(colored(f'[+] Scan result for: {tgtIP}', 'yellow'))
+
+    setdefaulttimeout(1)
+
+    for tgtPort in range(0,65536):
+        t = Thread(target=multiConnScan, args=(tgtHost, int(tgtPort)))
+        t.start()    
+
+####### Specific ports #######
+
 def connScan(tgtHost, tgtPort):
     try:
         sock = socket(AF_INET, SOCK_STREAM)
@@ -19,7 +51,7 @@ def portScanner(tgtHost, tgtPorts):
     try:
         tgtIP = gethostbyname(tgtHost)
     except:
-        print(f'Unknow host {tgtHost}')
+        print(colored(f'[-] Unknow host {tgtHost}', 'red'))
 
     try:
         tgtName = gethostbyaddr(tgtIP)
@@ -33,19 +65,28 @@ def portScanner(tgtHost, tgtPorts):
         t = Thread(target=connScan, args=(tgtHost, int(tgtPort)))
         t.start()
 
+####### Main #######
+
 def main():
-    parser = optparse.OptionParser('Usage of program: ' + '-H <target host> -p <target ports>')
+    usage = 'Usage: python3 %prog [option] arg'
+    parser = optparse.OptionParser(usage=usage)
     parser.add_option('-H', dest='tgtHost', type='string', help='specifiy target host')
     parser.add_option('-p', dest='tgtPort', type='string', help='specifiy target post seperate by comma')
+    parser.add_option('-a', dest='tgtPorts', action='store_true', default=True, help='scann all ports')
     (options, args) = parser.parse_args()
     tgtHost = options.tgtHost
     tgtPort = str(options.tgtPort).split(',')
+    tgtPorts = options.tgtPorts
 
-    if (tgtHost == None) | (tgtPort == None):
+    if tgtHost == None and tgtPort == None:
         print(parser.usage)
         exit(0)
+
+    if tgtHost != None and tgtPorts == True:
+        multiPortsScanner(tgtHost)
     
-    portScanner(tgtHost, tgtPort)
+    else:
+        portScanner(tgtHost, tgtPort)
 
 
 if __name__ == '__main__':
